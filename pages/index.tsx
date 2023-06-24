5f2a708650913b9971180aff2976b36f65fff9e6
tocchi <101105704+tocchi-739@users.noreply.github.com>
2023-06-24 14:08:15 +0900

    ✨ データの登録・表示機能を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,14 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
 const Home: NextPage = () => {
+  interface data {
+    weight: string;
+    fatPercent: string;
+    visceralFatLevel: string;
+    bmi: string;
+  }
+
+  const [data, setData] = useState<data[]>([]);
   const [record, setRecord] = useState({
     weight: "",
     fatPercent: "",
@@ -17,6 +25,15 @@ const Home: NextPage = () => {
       [e.target.name]: e.target.value,
     }));
   };
+  const handleClick = () => {
+    setData((data) => [...data, record]);
+    setRecord({
+      weight: "",
+      fatPercent: "",
+      visceralFatLevel: "",
+      bmi: "",
+    });
+  };
 
   return (
     <div className={styles.container}>
@@ -29,35 +46,62 @@ const Home: NextPage = () => {
         <h1>Health App!</h1>
       </header>
       <main>
-        <input
-          name="weight"
-          type="text"
-          placeholder="体重"
-          autoFocus={true}
-          onChange={handleChange}
-          value={record.weight}
-        />
-        <input
-          name="fatPercent"
-          type="text"
-          placeholder="体脂肪率"
-          onChange={handleChange}
-          value={record.fatPercent}
-        />
-        <input
-          name="visceralFatLevel"
-          type="text"
-          placeholder="内臓脂肪レベル"
-          onChange={handleChange}
-          value={record.visceralFatLevel}
-        />
-        <input
-          name="bmi"
-          type="text"
-          placeholder="BMI"
-          onChange={handleChange}
-          value={record.bmi}
-        />
+        <div>
+          <input
+            name="weight"
+            type="text"
+            placeholder="体重"
+            autoFocus={true}
+            onChange={handleChange}
+            value={record.weight}
+          />
+          <input
+            name="fatPercent"
+            type="text"
+            placeholder="体脂肪率"
+            onChange={handleChange}
+            value={record.fatPercent}
+          />
+          <input
+            name="visceralFatLevel"
+            type="text"
+            placeholder="内臓脂肪レベル"
+            onChange={handleChange}
+            value={record.visceralFatLevel}
+          />
+          <input
+            name="bmi"
+            type="text"
+            placeholder="BMI"
+            onChange={handleChange}
+            value={record.bmi}
+          />
+          <button onClick={handleClick}>登録</button>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>体重</th>
+                <th>体脂肪率</th>
+                <th>内臓脂肪レベル</th>
+                <th>BMI</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((d, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{d.weight}</td>
+                    <td>{d.fatPercent}</td>
+                    <td>{d.visceralFatLevel}</td>
+                    <td>{d.bmi}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </main>
 
       <footer></footer>
