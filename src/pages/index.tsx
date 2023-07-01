399a7c3ac364e05f32781169d66560f3be693192
tocchi <101105704+tocchi-739@users.noreply.github.com>
2023-07-01 14:44:07 +0900

    ♻️ 🐛  inputエリアで最大文字数まで入力後次のinputエリアをフォーカスする機能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,111 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
-import { useEffect, useState } from "react";
 import { InputArea } from "../components/InputArea";
 import { DisPlayArea } from "../components/DisplayArea";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
-import { app } from "../firebase/firebase";
 
-const db = getFirestore(app);
 const Home: NextPage = () => {
-  const [record, setRecord] = useState({
-    date: "",
-    weight: "",
-    fatPercent: "",
-    visceralFatLevel: "",
-    bmi: "",
-  });
-
-  const [date, setDate] = useState<Date>();
-  useEffect(() => {
-    setDate(new Date());
-  }, []);
-  useEffect(() => {
-    if (date) {
-      const yyyy = date.getFullYear();
-      const mm = ("0" + (date.getMonth() + 1)).slice(-2);
-      const dd = ("0" + date.getDate()).slice(-2);
-      const selectedDay = yyyy + "-" + mm + "-" + dd;
-      setRecord((prev) => ({
-        ...prev,
-        date: selectedDay,
-      }));
-    }
-  }, [date?.getDate()]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const target = e.target;
-    const maxLength = target.getAttribute("maxLength");
-    const currentLength = target.value.length;
-
-    if (currentLength >= Number(maxLength)) {
-      const nextInput = target.nextElementSibling;
-      if (nextInput instanceof HTMLInputElement) {
-        nextInput.focus();
-      }
-    } else {
-      setRecord((prev) => ({
-        ...prev,
-        [target.name]: target.value,
-      }));
-    }
-    if (target.name === "visceralFatLevel" && target.value.length > 1) {
-      alert("文字数オーバーです");
-      return;
-    } else if (target.name !== "visceralFatLevel" && target.value.length > 4) {
-      alert("文字数オーバーです");
-      return;
-    } else {
-      setRecord((prev) => ({
-        ...prev,
-        [target.name]: target.value,
-      }));
-    }
-  };
-  const handleChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newDate = new Date(e.target.value);
-    setDate(newDate);
-  };
-  const handleClick = async () => {
-    if (
-      record.date === "" ||
-      record.weight === "" ||
-      record.fatPercent === "" ||
-      record.visceralFatLevel === "" ||
-      record.bmi === ""
-    ) {
-      alert("未入力の箇所があります");
-      return;
-    }
-    const regex = /\d{2}\.\d/;
-    if (!regex.test(record.weight)) {
-      alert("体重の入力に誤りがあります");
-      return;
-    } else if (!regex.test(record.fatPercent)) {
-      alert("体脂肪率の入力に誤りがあります");
-      return;
-    } else if (!regex.test(record.bmi)) {
-      alert("BMIの入力に誤りがあります");
-      return;
-    }
-    const docRef = await addDoc(collection(db, "health-data"), {
-      date: record.date,
-      weight: record.weight,
-      fatPercent: record.fatPercent,
-      visceralFatLevel: record.visceralFatLevel,
-      bmi: record.bmi,
-    });
-    setRecord({
-      date: "",
-      weight: "",
-      fatPercent: "",
-      visceralFatLevel: "",
-      bmi: "",
-    });
-    window.location.reload();
-  };
-
   return (
     <>
       <Head>
@@ -118,12 +17,7 @@ const Home: NextPage = () => {
           <h1>Health App!</h1>
         </header>
         <main className={styles.main}>
-          <InputArea
-            handleChange={handleChange}
-            handleChangeDate={handleChangeDate}
-            handleClick={handleClick}
-            record={record}
-          />
+          <InputArea />
           <DisPlayArea />
         </main>
         <footer></footer>
