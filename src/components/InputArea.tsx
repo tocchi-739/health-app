ae11c07ed5cdd3eebd8f28fa5a37ef09f386e1c0
tocchi <101105704+tocchi-739@users.noreply.github.com>
2023-07-01 13:35:32 +0900

    ⚡️ inputラベルを追加

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -17,47 +17,68 @@ export const InputArea = (props: props) => {
   const { handleChange, handleChangeDate, handleClick, record } = props;
 
   return (
-    <div className={styles.inputArea}>
-      <input
-        name="date"
-        type="date"
-        onChange={handleChangeDate}
-        value={record.date}
-      />
-      <input
-        name="weight"
-        type="number"
-        placeholder="体重"
-        autoFocus={true}
-        onChange={handleChange}
-        value={record.weight}
-        maxLength={4}
-      />
-      <input
-        name="fatPercent"
-        type="number"
-        placeholder="体脂肪率"
-        onChange={handleChange}
-        value={record.fatPercent}
-        maxLength={4}
-      />
-      <input
-        name="visceralFatLevel"
-        type="number"
-        placeholder="内臓脂肪レベル"
-        onChange={handleChange}
-        value={record.visceralFatLevel}
-        maxLength={1}
-      />
-      <input
-        name="bmi"
-        type="number"
-        placeholder="BMI"
-        onChange={handleChange}
-        value={record.bmi}
-        maxLength={4}
-      />
+    <>
+      <div className={styles.inputArea}>
+        <div className={styles.inputItemWrapper}>
+          <label>日付</label>
+          <input
+            name="date"
+            type="date"
+            onChange={handleChangeDate}
+            value={record.date}
+          />
+        </div>
+        <div className={styles.inputItemWrapper}>
+          <label htmlFor="weight">体重</label>
+          <input
+            id="weight"
+            name="weight"
+            type="number"
+            placeholder="○○,○"
+            autoFocus={true}
+            onChange={handleChange}
+            value={record.weight}
+            maxLength={4}
+          />
+        </div>
+        <div className={styles.inputItemWrapper}>
+          <label htmlFor="fatPercent">体脂肪率</label>
+          <input
+            id="fatPercent"
+            name="fatPercent"
+            type="number"
+            placeholder="○○,○"
+            onChange={handleChange}
+            value={record.fatPercent}
+            maxLength={4}
+          />
+        </div>
+        <div className={styles.inputItemWrapper}>
+          <label htmlFor="visceralFatLevel">内臓脂肪レベル</label>
+          <input
+            id="visceralFatLevel"
+            name="visceralFatLevel"
+            type="number"
+            placeholder="○"
+            onChange={handleChange}
+            value={record.visceralFatLevel}
+            maxLength={1}
+          />
+        </div>
+        <div className={styles.inputItemWrapper}>
+          <label htmlFor="bmi">BMI</label>
+          <input
+            id="bmi"
+            name="bmi"
+            type="number"
+            placeholder="○○,○"
+            onChange={handleChange}
+            value={record.bmi}
+            maxLength={4}
+          />
+        </div>
+      </div>
       <Button text="登録" handleClick={handleClick} />
-    </div>
+    </>
   );
 };
